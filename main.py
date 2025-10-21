810796d0125e70bafa2a591b8a45bb74897d7128
Mahmudul Hasan <mahmudul@dinnova.io>
2025-10-21 02:21:31 -0700

    conditonal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from selectors import SelectSelector
+
 import pyttsx3
 import PyPDF2
 
@@ -12,8 +14,8 @@ for p in range(7, 14):
     page = pdfreader.getPage(p)
     text = book.read()
     test = page.extractText()
-    # friend.say(test)
-    # friend.runAndWait()
+    #friend.say(test)
+    #friend.runAndWait()
 
 print("hello world")
 
@@ -30,7 +32,7 @@ print(name, address, address1)
 
 firstNum = 10
 s = 0
-for x in range(0, 10):
+for x in range(0, 2):
     secondNum = input('SecondNum: ')
     s= s+x
     print("index: ", x,s)
@@ -39,3 +41,26 @@ for x in range(0, 10):
         print("valid number")
     else:
         print("invalid number")
+
+forSum = 0
+for i in  range(0,100,45):
+    forSum = forSum + i
+    print(i)
+
+evenNumber = 0
+odeNumber = 0
+totalSum = 0
+for i in  range(0,100):
+    if i%2==0:
+        continue
+    else:
+        odeNumber = odeNumber + i
+        print("Ode Number: ", i)
+
+    totalSum = evenNumber + odeNumber
+    print("Sum: ", totalSum)
+
+
+print("Total Sum: ", totalSum)
+print("Even Number: ", evenNumber)
+print("Ode Number: ", odeNumber)
